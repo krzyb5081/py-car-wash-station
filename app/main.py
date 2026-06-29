bbd16b5145f6216c7efbc280cb7286177b88f78b
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-06-29 14:07:49 +0200

    Develop calculate_washing_price()

--- app/main.py
+++ app/main.py
@@ -18,3 +18,8 @@ class CarWashStation:
             if car.clean_mark > self.clean_power:
                 income += 1
         return round(income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        price = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
+        return round(price, 1)
+
